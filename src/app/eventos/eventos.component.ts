7861572d2eb54ded18c1fdd4a0ecc029da7457a3
petinari <petinari@outlook.com>
2023-03-25 20:07:29 +0000

    25032023

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -7,16 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  eventoFiltrados: any = [];
+
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+  public set filtroLista(v: string) {
+    console.log(v);
+    this._filtroLista = v;
+    this.eventoFiltrados = this.filtrarEventos(this.filtroLista);
+    console.log(this.eventoFiltrados);
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLowerCase().includes(filtrarPor) ||
+        evento.local.toLowerCase().includes(filtrarPor)
+    );
+  }
+  public eventos: any = [];
+  widthImg = 150;
+  marginImg = 2;
+  mostrarImgBool = false;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.GetEventos();
   }
 
+  public mostrarImg() {
+    this.mostrarImgBool = !this.mostrarImgBool;
+  }
+
   public GetEventos(): any {
     this.http.get('https://localhost:7238/evento').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        this.eventos = response;
+        this.eventoFiltrados = this.eventos;
+      },
       (error) => console.log(error)
     );
   }
